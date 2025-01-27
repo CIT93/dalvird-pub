ef7dd3cd32d1f1f84df32b67cfc8cfeaa2871196
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-01-27 12:59:39 -0800

    week 3 attendance

--- w1/main.js
+++ w1/main.js
@@ -1,1 +1,20 @@
-console.log("Week 1 - Hello World");
+console.log("Hello from inside main.js file");
+
+//let myVar;
+
+const myVar = "helourrso";
+const myVarType = typeof(myVar);
+console.log("myVarType" + myVarType); //old way
+console.log(`myVarType ${myVarType}`); // new way
+
+function runNow () {
+    if (myVarType === "number") {
+        console.log(`will line 11 run?`);
+    }
+    else {
+        console.log(`will line 13 run?`);
+    }
+}
+
+runNow();
+runNow();
