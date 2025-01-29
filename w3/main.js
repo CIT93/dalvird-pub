499bc5d9d2e03aa833b4abb42fe42db72af3df8a
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-01-29 10:17:47 -0800

    working code

--- w3/main.js
+++ w3/main.js
@@ -1,6 +1,5 @@
 
 function determineHouseHoldPts(membersInHousehold) {
-
     // If you live alone, then add 14 points to your carbon footprint.
     if (membersInHousehold === 1) {
         cfp += 14;
@@ -26,7 +25,33 @@ function determineHouseHoldPts(membersInHousehold) {
     console.log(`Based on the number of members in the household ${membersInHousehold} the carbon points added are: ${cfp}.`)
 }
 
+function sizeOfHome(houseSize) {
+    let ptsAdded = 0;
+    // If you have a large house, then add 10 points to your score.
+    if (houseSize.toLowerCase() === "large") {
+        ptsAdded = 10;
+        cfp += ptsAdded;
+    } 
+    // If you have a medium-sized house, then add 7 points.
+    else if (houseSize.toLowerCase() === "medium") {
+        ptsAdded = 7;
+        cfp += ptsAdded;
+    }
+    // If you have a small house, then add 4 points.
+    else if (houseSize.toLowerCase() === "small") {
+        ptsAdded = 4;
+        cfp += ptsAdded;
+    }
+    // If you live in an apartment, then add 2 points.
+    else if (houseSize.toLowerCase() === "apt") {
+        ptsAdded = 2;
+        cfp += ptsAdded;
+    }
+    console.log(`Based on the size of your house ${houseSize} the carbon points are now: ${cfp}. Added: ${ptsAdded}`)
+}
+
 let cfp = 0;
 const membersInHousehold = 1;
-determineHouseHoldPts(3)
-determineHouseHoldPts(4)
+determineHouseHoldPts(3);
+sizeOfHome("large");
+//determineHouseHoldPts(4)
