ee40bbb2f30c2f29f4546a2fbcc55aacdb7bf359
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-04-03 20:48:43 -0700

    w12 my attempt at the code, before I review other students code.

---
+++ w12/README.md
@@ -0,0 +1,5 @@
+## Functions
+Parameters are variables that are used in the function and only can be used in the function (no scope outside of function), while arguments are variables we pass into the function when we call it. I used the word parameter and argument interchangeably, but now I know the difference. I already understood that we needed to declare and assign a variable to a function call to capture the return value. 
+- Default value: 
+- Spread argument:
+- IIFE:
---
+++ w12/main.js
@@ -0,0 +1,39 @@
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+//const TIMER = document.getElementById("timer");
+
+FORM.addEventListener("submit", function(e) {  //callback
+    e.preventDefault();
+
+    OUTPUT.textContent = "";
+
+    const eType = FORM.exercise.value;
+    const eReps = FORM.reps.value;
+    const eMins = FORM.minutes.value;
+
+    const start = document.createElement("h3");
+    start.textContent = `Start ${eType}! Goal: ${eReps} reps`;
+    OUTPUT.appendChild(start);
+
+    // const timer = document.createElement("p");
+    // timer.textContent = mins
+    // TIMER.appendChild(timer);
+
+    const miliTime = minutesToMili(eMins);
+    setTimer(workoutDone, miliTime);
+
+});
+
+ const setTimer = (callback, mili) => {
+  setTimeout(() => {
+    callback();
+  }, mili);
+}
+
+const minutesToMili = mins => mins * 2000; //60000
+
+function workoutDone() {
+  const end = document.createElement("h3");
+  end.textContent = "STOP!";
+  OUTPUT.appendChild(end);
+}
