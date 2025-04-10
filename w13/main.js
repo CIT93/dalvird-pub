f379d4b6aee1c4a4948011ee6becb9eefe9de04c
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-04-10 14:33:43 -0700

    w13 my attempt at the code, before I review other students or solution code.

---
+++ w13/main.js
@@ -0,0 +1,51 @@
+const FORM = document.getElementById("form");
+const OUTPUT = document.getElementById("output");
+
+FORM.addEventListener("submit", function(e) {
+    e.preventDefault();
+
+    OUTPUT.textContent = "";
+
+    const eType = FORM.exercise.value;
+    const eReps = FORM.reps.value;
+    const eMins = FORM.minutes.value;
+
+    const start = document.createElement("h3");
+    start.textContent = `Start ${eType}! Goal: ${eReps} reps`;
+    OUTPUT.appendChild(start);
+
+    const milliseconds = minutesToMilliseconds(eMins);    // converting minutes to milliseconds
+    timer(milliseconds).then(workoutDone)
+    .catch(error)
+
+    FORM.reset();
+
+});
+
+const timer = (ms) => {
+  return new Promise(function(resolve, reject){
+    setTimeout(() => {
+      resolve("Workout complete");
+         }, ms);
+  })
+}
+
+const error = () => {
+  const end = document.createElement("h3");
+  end.textContent = `Error`;
+  OUTPUT.appendChild(end);
+}
+
+//  const setTimer = (callback, mili) => {
+//   setTimeout(() => {
+//     callback();
+//   }, mili);
+// }
+
+const minutesToMilliseconds = mins => mins * 2000; //60000 if a minute, 2000 == 2 seconds
+
+function workoutDone(message) {
+  const end = document.createElement("h3");
+  end.textContent = `STOP! ${message}`;
+  OUTPUT.appendChild(end);
+}
