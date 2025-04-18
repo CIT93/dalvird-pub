f8b0b82592788f0e415568825bd9b9359d369264
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-04-18 15:40:26 -0700

    fetch api code and notes

--- w14/main.js
+++ w14/main.js
@@ -1,80 +1,40 @@
-// function getData() {
-//     return new Promise(resolve => {
-//         setTimeout(() => {
-//             resolve(46);
-//         }, 1);
-//     })
+// const url = 'http://worldtimeapi.org/api/timezone/America/New_York';
+
+// async function getData() {
+//     const response = await fetch(url);
+//     data = await response.json();
+//     console.log(data);
 // }
 
-// async function start() {
-//     const data = await fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast');
-//     const result = await data.json();
-//     console.log(result.properties.periods[1].shortForecast);
+// getData();
+
+const url = 'https://api.spotify.com/v1/artists/2P1puQXmG48EVLBrHbum11';
+const request = new Request (url, {
+    headers: {
+        'Authorization': 'Bearer 123'
+    }
+})
+//     // const response = await fetch(request);
+//     // const data = await response.json();
+//     // console.log(data);
+//     fetch().then(response => response.json())
+//     .then(data => console.log(data))
+//     .catch(error => console.log(`Error ${error}`))
 // }
 
-// async function start2() {
-//     fetch('https://api.weather.gov/gridpoints/OKX/35,35/forecast')
-//     .then(data => data.json())
-//     .then(result => {
-//         console.log(result.properties.periods[1].shortForecast);
-//     })
-// }
-
-// async function start2() {
-//     getData().then(result => {
-//         console.log(result);
-//     })
-// }
-
-
-//start2();
-
-// function getData() {
-//     return new Promise((resolve, reject) => {
-//         setTimeout(() => {
-//             reject('something wrong!');
-//         }, 1);
-//     })
-// }
-
-// async function start() {
-//     try {
-//     // success
-//     const result = await getData();
-//     }
-//     catch (error) {
-//     //failure
-//     console.log(error);
-//     }
-// }
-
-// async function start2() {
-//     const result = await getData()
-//     .catch(error => {
-//         console.log(error); 
-//     }) 
-//     console.log(error);
-// }
-
-//start();
-
-
-async function start() {
+async function getData() {
     try {
-        const data = await fetch('https://api.weather.gov/gridpoints/HNX/52,100/forecast');
-        const result = await data.json();
-        onSucc(result.properties.periods[1].temperature);
-    } catch(e) {
-        onFailure();
+        const response = await fetch(request);
+        const data = await response.json();
+        if (response.status === 200) {
+            console.log('Success', data);
+        }
+        else {
+            console.log('Server Error', data.error.message);
+        }
+    } catch (error) {
+        console.log('Error', error);
     }
 }
 
-const onSucc = (result) => {
-    console.log(result);
-}
-
-const onFailure = (e) => {
-    console.log(e);
-}
-
-start();
+getData();
