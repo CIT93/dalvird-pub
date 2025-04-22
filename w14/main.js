2afab2fc52c6c3d986e2b8c180a601d43f301455
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-04-21 21:56:18 -0700

    added random images and fixed linked issue with new website

--- w14/main.js
+++ w14/main.js
@@ -1,7 +1,8 @@
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
 const urlComment = 'https://jsonplaceholder.typicode.com/comments/1';
-const urlPhotoArr = 'https://jsonplaceholder.typicode.com/photos';
+const urlPhotoArr = 'https://picsum.photos/v2/list';
 const output = document.getElementById("output");
+const outputText = document.getElementById("outputText");
 // const url = 'https://invalid.url.typicode.com/bad-endpoint'; // Catch error
 // const url = 'https://jsonplaceholder.typicode.com/todos/1ggggg'; // server error
 
@@ -12,8 +13,8 @@ async function getTodo() {
         //console.log(data);
         //console.log(response);
         if (response.status === 200) {
-            console.log("Success: " + JSON.stringify(data));
-            output.textContent = `To do: ${JSON.stringify(data)}`;
+            //console.log("Success: " + JSON.stringify(data));
+            outputText.textContent = `To do: ${JSON.stringify(data)}`;
         }
         else {
             console.log('Endpoint Error: ' + response.statusText);
@@ -23,34 +24,37 @@ async function getTodo() {
     }
 }
 
-const getComment = async() => {
-    try {
-        const comment = await fetch(urlComment);
-        const commentJson = await comment.json();
-        if (comment.status === 200) {
-            console.log("Success: " + JSON.stringify(commentJson));
-            output.textContent += `Comment: ${JSON.stringify(commentJson)}`;
-        }
-        else {
-            console.log('Endpoint Error: ' + comment.statusText);
-        }
-    }
-    catch (error) {
-        console.log(" Server Error " + error);
-    }
-}
+// const getComment = async() => {
+//     try {
+//         const comment = await fetch(urlComment);
+//         const commentJson = await comment.json();
+//         if (comment.status === 200) {
+//             //console.log("Success: " + JSON.stringify(commentJson));
+//             output.textContent += `Comment: ${JSON.stringify(commentJson)}`;
+//         }
+//         else {
+//             console.log('Endpoint Error: ' + comment.statusText);
+//         }
+//     }
+//     catch (error) {
+//         console.log(" Server Error " + error);
+//     }
+// }
 
 const getPhotos = async() => {
     try {
         let photos = [];
-        photos = await fetch(urlPhotoArr);
+        let randomNum = Math.floor(Math.random() * (10 - 1 + 1)) + 1;
+        console.log(randomNum);
+        photos = await fetch(`${urlPhotoArr}?page=${randomNum}`);
         let p = await photos.json();
         //console.log(JSON.stringify(p));
         if (photos.status === 200) {
-            p.slice(0,5).forEach((photo) => {
+            p.forEach((photo) => {
                 const img = document.createElement("img");
-                console.log(photo.thumbnailUrl);
-                img.setAttribute("src", photo.url);
+                img.setAttribute("src", photo.download_url);
+                img.setAttribute("width", 250);
+                img.setAttribute("height", 150);
                 output.appendChild(img);
             });
         }
