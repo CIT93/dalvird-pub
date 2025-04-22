c956c604a564ffb1725019ff77ada446e47461f6
Dalvir Dhaliwal <DalvirKD05@gmail.com>
2025-04-21 17:32:08 -0700

    adding photos to the todo

--- w14/main.js
+++ w14/main.js
@@ -1,8 +1,11 @@
 const url = 'https://jsonplaceholder.typicode.com/todos/1';
+const urlComment = 'https://jsonplaceholder.typicode.com/comments/1';
+const urlPhotoArr = 'https://jsonplaceholder.typicode.com/photos';
+const output = document.getElementById("output");
 // const url = 'https://invalid.url.typicode.com/bad-endpoint'; // Catch error
 // const url = 'https://jsonplaceholder.typicode.com/todos/1ggggg'; // server error
 
-async function getData() {
+async function getTodo() {
     try {
         const response = await fetch(url);
         const data = await response.json();
@@ -10,22 +13,64 @@ async function getData() {
         //console.log(response);
         if (response.status === 200) {
             console.log("Success: " + JSON.stringify(data));
-            renderData(data);
+            output.textContent = `To do: ${JSON.stringify(data)}`;
         }
         else {
-            console.log('Server Error: ' + response.statusText);
+            console.log('Endpoint Error: ' + response.statusText);
         }
     } catch(error) {
         console.log('Error ' + error);
     }
 }
 
-const renderData = (data) => {
-    // const output = document.body;
-    // output.textContent = JSON.stringify(data);
-    document.body.textContent = JSON.stringify(data);
+const getComment = async() => {
+    try {
+        const comment = await fetch(urlComment);
+        const commentJson = await comment.json();
+        if (comment.status === 200) {
+            console.log("Success: " + JSON.stringify(commentJson));
+            output.textContent += `Comment: ${JSON.stringify(commentJson)}`;
+        }
+        else {
+            console.log('Endpoint Error: ' + comment.statusText);
+        }
+    }
+    catch (error) {
+        console.log(" Server Error " + error);
+    }
 }
 
-getData();
+const getPhotos = async() => {
+    try {
+        let photos = [];
+        photos = await fetch(urlPhotoArr);
+        let p = await photos.json();
+        //console.log(JSON.stringify(p));
+        if (photos.status === 200) {
+            p.slice(0,5).forEach((photo) => {
+                const img = document.createElement("img");
+                console.log(photo.thumbnailUrl);
+                img.setAttribute("src", photo.url);
+                output.appendChild(img);
+            });
+        }
+        else {
+            console.log('Endpoint Error: ' + photos.statusText);
+        }
+    }
+    catch (error) {
+        console.log(" Server Error " + error);
+    }
+}
+
+
+
+const renderData = (data) => {
+    
+}
+
+getTodo();
+//getComment();
+getPhotos();
 
 
